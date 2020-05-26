28e8e699b34162777f810660dfa79577a9803ed1
Jessica Schaefer <jschaefer@3pc.de>
2020-05-26 10:13:42 +0200

    pundit controller policy

--- app/controllers/sloths_controller.rb
+++ app/controllers/sloths_controller.rb
@@ -1,16 +1,19 @@
 class SlothsController < ApplicationController
   before_action :set_sloth, only: [ :show, :edit, :update, :destroy ]
   def index
-    @sloths = Sloth.all
+    @sloths = policy_scope(Sloth)
   end
 
   def show
   end
 
   def new
+    @sloth = Sloth.new
+    authorize @sloth
   end
 
   def create
+    authorize @sloth
   end
 
   def edit
@@ -28,6 +31,7 @@ class SlothsController < ApplicationController
 
   def set_sloth
     sloth = Sloth.find(params[:id])
+    authorize @sloth
   end
 
   def sloth_params
---
+++ app/policies/sloth_policy.rb
@@ -0,0 +1,23 @@
+class SlothPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def create?
+      true
+    end
+
+    def show?
+      true
+    end
+
+    def update?
+      user == record.user
+    end
+
+    def destroy?
+      user == record.user
+    end
+  end
+end
