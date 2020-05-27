32c09dd3c2634732a64cefe6aea54743ef84e58b
Jessica Schaefer <jschaefer@3pc.de>
2020-05-27 11:30:42 +0200

    sloth index and add geo gem

--- app/models/sloth.rb
+++ app/models/sloth.rb
@@ -5,5 +5,6 @@ class Sloth < ApplicationRecord
   validates :name, :address, :price, presence: true
 
   has_many_attached :photos
-
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
