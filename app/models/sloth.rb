5f3123f89e87a021f9a4b07f5db979840b326290
aamruth shankar <aamruth@hotmail.com>
2020-05-28 17:09:03 +0200

    filter

--- app/models/sloth.rb
+++ app/models/sloth.rb
@@ -8,12 +8,4 @@ class Sloth < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   has_many_attached :photos
-
-  # include PgSearch::Model
-  # pg_search_scope :search_by_address,
-  #   against: [ :address ],
-  #   using: {
-  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  #   }
-
 end
